bf18f555d29a9fcaab2eba8e3622e039a084843f
holodome <ilal.vinogradov@gmail.com>
2022-03-24 21:27:10 +0300

    add middleware logger

--- src/startup.rs
+++ src/startup.rs
@@ -4,15 +4,17 @@ use actix_web::{web, App, HttpServer};
 use diesel::r2d2::{self, ConnectionManager};
 use diesel::SqliteConnection;
 use std::net::TcpListener;
+use actix_web::middleware::Logger;
 
 pub type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 
 pub fn run(listener: TcpListener, pool: Pool) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(move || {
         App::new()
-            .route("/health_check", web::get().to(health_check))
+            .wrap(Logger::default())
             .app_data(web::Data::new(pool.clone()))
-    })
+            .route("/health_check", web::get().to(health_check))
+        })
     .listen(listener)?
     .run();
     Ok(server)
