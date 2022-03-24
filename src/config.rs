9274e15103eb98b58bb13a4d3ee3bd6267ede06f
holodome <ilal.vinogradov@gmail.com>
2022-03-24 18:01:17 +0300

    create config

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,19 @@
+use config::{Config, ConfigBuilder};
 use secrecy::Secret;
 
 #[derive(Clone, serde::Deserialize)]
 pub struct HmacSecret(pub Secret<String>);
 
 #[derive(Clone, serde::Deserialize)]
-pub struct AppSettings {
+pub struct Settings {
     pub hmac_secret: HmacSecret,
+    pub database_path: String,
+    pub application_port: u16
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let config = Config::builder()
+        .add_source(config::File::with_name("config"))
+        .build()?;
+    config.try_deserialize()
 }
