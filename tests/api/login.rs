403ef360ecbda99de935e116054cebdc1ae57d74
holodome <ilal.vinogradov@gmail.com>
2022-03-30 16:02:30 +0300

    slightly change test api

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -1,1 +1,8 @@
+use crate::helpers::{assert_is_redirect_to, TestApp};
 
+#[actix_web::test]
+async fn logout_returns_redirect_to_login_when_not_logged_in() {
+    let app = TestApp::spawn().await;
+    let response = app.post_logout().await;
+    assert_is_redirect_to(&response, "/login")
+}
