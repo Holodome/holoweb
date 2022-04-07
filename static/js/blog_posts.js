ca5b977d191f6e5d9185e5b005cd0d67ab765d3b
holodome <ilal.vinogradov@gmail.com>
2022-04-07 19:28:51 +0300

    create test pagination js script for blog posts

---
+++ static/js/blog_posts.js
@@ -0,0 +1,27 @@
+get_query = () => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    return Object.fromEntries(urlSearchParams.entries());
+}
+
+set_pagination_param = (params, n) => {
+    params["size"] = n;
+}
+
+redirect_on_same_page_with_query = query => {
+    window.location.href = window.location.origin + window.location.pathname +
+        "?" + new URLSearchParams(query).toString();
+}
+
+pagination_function = n => {
+    return () => {
+        let query = get_query();
+        set_pagination_param(query, n);
+        redirect_on_same_page_with_query(query);
+    };
+}
+
+window.onload = () => {
+    document.getElementById("paginate_10").onclick = pagination_function(10);
+    document.getElementById("paginate_20").onclick = pagination_function(20);
+    document.getElementById("paginate_40").onclick = pagination_function(40);
+}
