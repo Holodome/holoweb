8495eb8472e728b70fb5e8a83f0382240bb468a3
holodome <ilal.vinogradov@gmail.com>
2022-04-13 14:04:05 +0300

    all comment tests are passing now

--- tests/api/comments.rs
+++ tests/api/comments.rs
@@ -149,15 +149,62 @@ async fn all_comments_for_blog_post_are_displayed_correctly() {
 
 #[tokio::test]
 async fn create_response_comment_works() {
-    todo!()
-}
+    let app = TestApp::spawn().await;
+    let test_user = TestUser::generate();
+    let user_id = test_user.register_internally(&app.pool);
+    test_user.login(&app).await;
+    let blog_post = TestBlogPost::generate();
+    let blog_post_id = blog_post.register_internally(&app.pool, &user_id);
+    let test_comment = TestComment::generate();
+    let comment_id = test_comment.register_internally(&app.pool, &blog_post_id, &user_id);
+    let other_comment = TestComment::generate();
 
-#[tokio::test]
-async fn displaying_response_tree_works() {
-    todo!()
+    let response = app
+        .post_create_comment(
+            &serde_json::json!({
+                "contents": &other_comment.contents,
+                "parent": comment_id.as_ref()
+            }),
+            &blog_post_id,
+        )
+        .await;
+
+    assert_is_redirect_to(&response, &format!("/blog_posts/{}", blog_post_id.as_ref()));
+    let post_html = app
+        .get_view_blog_post_page_html(blog_post_id.as_ref())
+        .await;
+    assert!(post_html.contains(&other_comment.contents));
+    assert!(post_html.contains(&test_comment.contents));
 }
 
 #[tokio::test]
 async fn delete_comment_in_middle_of_response_tree_works() {
-    todo!()
+    let app = TestApp::spawn().await;
+    let test_user = TestUser::generate();
+    let user_id = test_user.register_internally(&app.pool);
+    test_user.login(&app).await;
+    let blog_post = TestBlogPost::generate();
+    let blog_post_id = blog_post.register_internally(&app.pool, &user_id);
+    let test_comment = TestComment::generate();
+    let comment_id = test_comment.register_internally(&app.pool, &blog_post_id, &user_id);
+    let response_comment = TestComment::generate();
+    response_comment.register_response_internally(&app.pool, &blog_post_id, &user_id, &comment_id);
+
+    let response = app
+        .post_edit_comment(
+            &serde_json::json!({
+                "contents": "New contents",
+                "is_deleted": true
+            }),
+            &blog_post_id,
+            &comment_id,
+        )
+        .await;
+    assert_is_redirect_to(&response, &format!("/blog_posts/{}", blog_post_id.as_ref()));
+    let post_html = app
+        .get_view_blog_post_page_html(blog_post_id.as_ref())
+        .await;
+
+    assert!(post_html.contains(&response_comment.contents));
+    assert!(!post_html.contains(&test_comment.contents));
 }
--- tests/helpers/test_comment.rs
+++ tests/helpers/test_comment.rs
@@ -32,4 +32,22 @@ impl TestComment {
             .expect("Failed to insert comment.html")
             .id
     }
+
+    pub fn register_response_internally(
+        &self,
+        pool: &Pool,
+        post_id: &BlogPostID,
+        author_id: &UserID,
+        parent_id: &CommentID,
+    ) -> CommentID {
+        let new_comment = NewComment {
+            author_id,
+            post_id,
+            parent_id: Some(parent_id),
+            contents: &self.contents,
+        };
+        insert_new_comment(&pool, &new_comment)
+            .expect("Failed to insert comment.html")
+            .id
+    }
 }
