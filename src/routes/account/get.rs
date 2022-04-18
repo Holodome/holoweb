3cfb451762f1ee7d916affc2fae0f16d469a81ff
holodome <ilal.vinogradov@gmail.com>
2022-04-18 09:42:05 +0300

    create stubs for rust part of account page

--- src/routes/account/get.rs
+++ src/routes/account/get.rs
@@ -2,16 +2,43 @@ use crate::domain::users::{UserID, UserName};
 use crate::services::get_user_by_id;
 use crate::utils::{e500, render_template};
 
+use crate::domain::blog_posts::BlogPost;
+use crate::domain::projects::Project;
 use crate::Pool;
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::IncomingFlashMessages;
 use askama::Template;
 
+struct ProjectInfo<'a> {
+    id: &'a str,
+    title: &'a str,
+    brief: &'a str,
+    role: &'a str,
+}
+
+struct BlogPostInfo<'a> {
+    id: &'a str,
+    title: &'a str,
+    brief: &'a str,
+    role: &'a str,
+}
+
+struct CommentInfo<'a> {
+    project: &'a Project,
+    blog_post: &'a BlogPost,
+    date: &'a str,
+    contents: &'a str,
+}
+
 #[derive(Template)]
 #[template(path = "account.html")]
-struct AccountPage {
+struct AccountPage<'a> {
     messages: IncomingFlashMessages,
-    user_name: UserName,
+    projects: Vec<ProjectInfo<'a>>,
+    blog_posts: Vec<BlogPostInfo<'a>>,
+    comments: Vec<CommentInfo<'a>>,
+    name: &'a str,
+    email: &'a str,
 }
 
 #[tracing::instrument(skip(pool, messages))]
@@ -20,13 +47,16 @@ pub async fn account(
     user_id: UserID,
     messages: IncomingFlashMessages,
 ) -> actix_web::Result<HttpResponse> {
-    let user_name = get_user_by_id(&pool, &user_id)
+    let user = get_user_by_id(&pool, &user_id)
         .map_err(e500)?
-        .ok_or_else(|| e500("Failed to get user name"))?
-        .name;
+        .ok_or_else(|| e500("Failed to get user name"))?;
 
     render_template(AccountPage {
         messages,
-        user_name,
+        projects: vec![],
+        blog_posts: vec![],
+        comments: vec![],
+        name: &user.name.as_ref(),
+        email: &user.email.as_ref(),
     })
 }
